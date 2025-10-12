921ad66e57afc04a253925375ec63bd8796331e3
fajruddinskin <55961345+fajruddinskin@users.noreply.github.com>
2025-10-12 06:47:34 -0700

    beans creation updated

--- src/main/java/com/medicalstore/MedicalStoreApplication.java
+++ src/main/java/com/medicalstore/MedicalStoreApplication.java
@@ -2,8 +2,10 @@ package com.medicalstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:system-spring-beans.xml")
 public class MedicalStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicalStoreApplication.class, args);
